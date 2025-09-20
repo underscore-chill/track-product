bd89db00e2503c19601139741bc52cf5f0f2c961
kellyncodes <kellyncodes@gmail.com>
2025-09-20 13:53:19 +0100

    chore

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   baseUrl: 'http://localhost:3000',
 
-  whatsappNumber: '+527716298887',
+  whatsappNumber: '+525574820445, +525568700013',
   email: 'seawaysdeliveryinc@yahoo.com',
 };
