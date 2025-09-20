8b9c9cb1a10dd309be93990738be49e2de502c62
kellyncodes <kellyncodes@gmail.com>
2025-09-20 13:38:45 +0100

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   baseUrl: 'https://sea-way-api.onrender.com',
-  whatsappNumber: '+527716298887',
+  whatsappNumber: '+525574820445, +525568700013',
   email: 'seawaysdeliveryinc@yahoo.com',
 };
