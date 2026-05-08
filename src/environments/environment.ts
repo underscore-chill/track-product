c0be731d703d7bc9487233ecf221fe322983188c
underscore-chill <seawaysdeliveryinc@yahoo.com>
2026-05-08 18:32:12 +0100

    Update baseUrl for production environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://sea-way-api.onrender.com',
+  baseUrl: 'https://track-product-api.onrender.com',
   whatsappNumber: '+525574820445, +525568700013',
   email: 'seawaysdeliveryinc@yahoo.com',
 };
